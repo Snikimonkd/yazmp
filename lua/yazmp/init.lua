763bd645fb2378154484e9ab901f072fd74b2e34
Lukash Sergey Davidovich <slukash@ozon.ru>
2023-12-08 19:47:27 +0300

    toggle feature + keep focus on original windof

--- lua/yazmp/init.lua
+++ lua/yazmp/init.lua
@@ -1,19 +1,25 @@
 local M = {}
-print("zenmode loaded")
 
 function M.zenmode(c)
-	local buf = vim.api.nvim_create_buf(false, false)
+	if M.buf == nil then
+		local cur_win = vim.fn.winnr("$")
+		M.buf = vim.api.nvim_create_buf(false, false)
 
-	vim.api.nvim_command("vsp")
-	vim.api.nvim_command("wincmd H")
-	pcall(vim.cmd, "buffer " .. buf)
+		vim.api.nvim_command("vsp")
+		vim.api.nvim_command("wincmd H")
+		pcall(vim.cmd, "buffer " .. M.buf)
 
-	local width = 30
-	if #c.fargs == 1 then
-		width = tonumber(c.fargs[1])
+		local width = 30
+		if #c.fargs == 1 then
+			width = tonumber(c.fargs[1])
+		end
+		vim.api.nvim_win_set_width(0, width)
+
+		vim.cmd.wincmd("w")
+	else
+		vim.api.nvim_buf_delete(M.buf, { force = true })
+		M.buf = nil
 	end
-
-	vim.api.nvim_win_set_width(0, width)
 end
 
 return M
